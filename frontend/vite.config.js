72ff5b3ad929e13979630bc1136fbc1c56fb0fe5
brandon <brandon.lim.2022@scis.smu.edu.sg>
2024-11-28 21:33:05 +0800

    Revert "fix vercel error"
    This reverts commit 9eadec12d1c9cf78ea7b022dee1a139645fc87b8.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Default output directory for Vite
-  },
-  server: {
-    historyApiFallback: true, // Dev server fallback for client-side routing
-  },
 })
