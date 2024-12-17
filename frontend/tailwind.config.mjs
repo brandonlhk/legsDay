983b8956f809843ea8b3f78c41bd13ec35e4abac
brandonlhk <brandon.lim.2022@scis.smu.edu.sg>
2024-12-17 23:30:37 +0800

    Renaming tailwind.css.js

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "themeGreen": "#00C9A7",
+        "blueGreen": "#476380",
+        "tertGreen": "#00967D",
+        "blueGrey": "#F5F7FA"
+      }
+    }
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light"],
+  },
+};
