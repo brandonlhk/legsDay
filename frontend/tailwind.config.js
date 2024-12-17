b00959e519585e196e2f1cfa51a385c54fc96dbc
brandonlhk <brandon.lim.2022@scis.smu.edu.sg>
2024-12-17 23:28:49 +0800

    ESM syntax for tailwind css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {    
-      colors: {
-        "themeGreen" : "#00C9A7",
-        "blueGreen" : "#476380",
-        "tertGreen" : "#00967D",
-        "blueGrey" : "#F5F7FA"
-    }},
-
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      "themeGreen": "#00C9A7",
+      "blueGreen": "#476380",
+      "tertGreen": "#00967D",
+      "blueGrey": "#F5F7FA"
+    }
   },
-  plugins: [
-    require('daisyui')
-  ],
-  daisyui: {
-    themes: ["light"]
-  }
-}
+};
+export const plugins = [
+  require('daisyui')
+];
+export const daisyui = {
+  themes: ["light"]
+};
